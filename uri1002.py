8a2619a5a8b7d9fe45be44c5cfc42f8f4909084f
FranciscoGJR <franciscoogjr@gmail.com>
2021-05-07 14:38:33 -0300

    Create uri1002.py

---
+++ uri1002.py
@@ -0,0 +1,11 @@
+'''
+name: Francisco Olivera Gomes Junior
+date: 07/06/2021
+url: https://www.urionlinejudge.com.br/judge/en/problems/view/1002
+'''
+
+raio = float(input())
+
+area = raio * raio * 3.14159
+
+print("A=%.4f" %area )
