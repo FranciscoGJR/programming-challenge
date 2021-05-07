e12145399c95b9595a8c390fd13d271acfcbd2ae
FranciscoGJR <franciscoogjr@gmail.com>
2021-05-07 15:55:57 -0300

    Create uri1003.py

---
+++ uri1003.py
@@ -0,0 +1,12 @@
+'''
+name: Francisco Olivera Gomes Junior
+date: 07/06/2021
+url: https://www.urionlinejudge.com.br/judge/en/problems/view/1003
+'''
+
+x1 = int(input())
+x2 = int(input())
+
+soma = x1 + x2
+
+print('SOMA = %i' %soma)
