eab93ef9835a8bed9de4fc2cff73f457801668af
FranciscoGJR <franciscoogjr@gmail.com>
2021-05-08 11:48:44 -0300

    uri1004

---
+++ uri1004.py
@@ -0,0 +1,11 @@
+'''
+name: Francisco Olivera Gomes Junior
+date: 07/06/2021
+url: https://www.urionlinejudge.com.br/judge/en/problems/view/1004
+'''
+x1 = int(input())
+x2 = int(input())
+
+product = x1 * x2
+
+print("PROD = %d" %product)
