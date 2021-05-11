c7c4597b311ae24a2adde760dd5ca26ea1cdf901
FranciscoGJR <franciscoogjr@gmail.com>
2021-05-10 23:48:21 -0300

    Create uri1005.py

---
+++ uri1005.py
@@ -0,0 +1,12 @@
+'''
+name: Francisco Olivera Gomes Junior
+date: 10/06/2021
+url: https://www.urionlinejudge.com.br/judge/en/problems/view/1005
+'''
+
+x1 = float(input())
+x2 = float(input())
+
+media = ((x1 *3.5) + (x2 * 7.5))/11
+
+print("MEDIA = %.5f" %media)
