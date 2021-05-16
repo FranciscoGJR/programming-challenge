96c706f860ad9dca904e9659ea0b045de5207f8a
FranciscoGJR <franciscoogjr@gmail.com>
2021-05-16 11:08:25 -0300

    Create uri1008.py

---
+++ uri1008.py
@@ -0,0 +1,13 @@
+'''
+name: Francisco Olivera Gomes Junior
+date: 16/06/2021
+url: https://www.urionlinejudge.com.br/judge/en/problems/view/1008
+'''
+
+numberEmployee = int(input())
+hours = int(input())
+cash = float(input())
+salary = hours * cash
+
+print("NUMBER = %i" %numberEmployee)
+print("SALARY = U$ %.2f" %salary)
