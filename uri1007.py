5e1eef4eee07ebf072158c78db0a23d2d1af59f6
FranciscoGJR <franciscoogjr@gmail.com>
2021-05-16 10:31:20 -0300

    Update uri1007.py

---
+++ uri1007.py
@@ -0,0 +1,12 @@
+'''
+name: Francisco Olivera Gomes Junior
+date: 16/06/2021
+url: https://www.urionlinejudge.com.br/judge/en/problems/view/1007
+'''
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print("DIFERENCA = %i" %(a * b - c * d))
