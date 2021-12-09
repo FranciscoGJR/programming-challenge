614c0f213b50a29b8d9b189f8a7976a7c569bb95
FranciscoGJR <franciscoogjr@gmail.com>
2021-12-09 11:09:36 -0300

    Folder

---
+++ URI/uri1010/uri1010.py
@@ -0,0 +1,18 @@
+'''
+inputs:
+    codeProduct1
+    co
+'''
+
+
+codeProduct1 = int(input())
+numberProduct1 = int(input())
+priceProduct1 = float(input())
+
+codeProduct2 = int(input())
+numberProduct2 = int(input())
+priceProduct2 = float(input())
+
+priceTotal = float(numberProduct1 * priceProduct1 + numberProduct2 * priceProduct2)
+
+print(princeToral)
