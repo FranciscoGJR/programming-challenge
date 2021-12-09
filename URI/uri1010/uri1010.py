9eb75197d39f8cf735ac12d76abf53197113798f
FranciscoGJR <franciscoogjr@gmail.com>
2021-12-09 16:22:26 -0300

    uri1010

--- URI/uri1010/uri1010.py
+++ URI/uri1010/uri1010.py
@@ -1,18 +1,20 @@
+line1 = input().split(" ")
+line2 = input().split(" ")
+
+code1 = int(line1[0])
+amount1 = int(line1[1])
+price1 = float(line1[2])
+
+code2 = int(line2[0])
+amount2 = int(line2[1])
+price2 = float(line2[2])
+
+total = price1 * amount1 + price2 * amount2
 '''
-inputs:
-    codeProduct1
-    co
+print(line1[0])
+print(code1)
 '''
 
+#print("VALOR A PAGAR: R$ {:.2}".format(total))
+print("VALOR A PAGAR: R$ %0.2f"%total)
 
-codeProduct1 = int(input())
-numberProduct1 = int(input())
-priceProduct1 = float(input())
-
-codeProduct2 = int(input())
-numberProduct2 = int(input())
-priceProduct2 = float(input())
-
-priceTotal = float(numberProduct1 * priceProduct1 + numberProduct2 * priceProduct2)
-
-print(princeToral)
