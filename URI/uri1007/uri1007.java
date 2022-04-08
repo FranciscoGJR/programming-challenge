73cf0960d2c5abd353dff723f7cf9453666717dd
FranciscoGJR <80439645+FranciscoGJR@users.noreply.github.com>
2022-04-08 09:59:11 -0300

    Create uri1007.java

---
+++ URI/uri1007/uri1007.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int D = sc.nextInt();
+		
+		int dif = (A * B) - (C * D); 
+		
+		System.out.printf("DIFERENCA = %d\n", dif);
+ 
+    }
+}
