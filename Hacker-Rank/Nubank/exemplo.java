bb00146cb72ffe49863759ead03ffc396b33c342
FranciscoGJR <franciscoogjr@gmail.com>
2023-09-25 19:42:48 -0300

    [Hacker-Rank] - Adicionando desafio Nubank

---
+++ Hacker-Rank/Nubank/exemplo.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class exemplo{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
+
+
+class Resolucao{
+
+    //private
+    //private
+
+    Resolucao(){
+
+    }
+
+    public void resposta(){
+
+
+    }
+
+    public String toString(){
+
+        return "";
+    }
+
+}
